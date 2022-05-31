a15662e27849c4e7a4f45d01ec4363e3ebee7562
José Fredes <jose.fredes@mayor.cl>
2022-05-31 02:27:02 -0400

    feat: created card component

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,32 +1,33 @@
-import React from 'react'
-import { getPokemonImage } from '../utils/getPokemonImage';
-import './pokemonCard.css'
-
+import React, { useState } from "react";
+import { getPokemonImage } from "../utils/getPokemonImage";
+import "./pokemonCard.css";
 
 type pokemonProps = {
-    id: string
-    name: string;
-    url: string;
-}
+  id: string;
+  name: string;
+  url: string;
+};
 
-const PokemonCard = ({id,name, url}: pokemonProps) => {
-   const idPokemon = (id.toString()).padStart(3, '0');
-    const pokemonImg =  getPokemonImage(idPokemon);
+const PokemonCard = ({ id, name, url }: pokemonProps) => {
+  const idPokemon = id.toString().padStart(3, "0");
+  const pokemonImg = getPokemonImage(idPokemon);
 
   return (
-    <div className='pokemon-card'>
-        <div>
-            <img className='image' src={pokemonImg} alt={name} />
-        </div>
-        <div>
-            <h3>{name}</h3>
-            <h3>#{idPokemon}</h3>
-        </div>
-        <div>
-            <button>Ver más</button>
-        </div>
+    <div className="pokemon-card">
+      <div>
+        <img className="image" src={pokemonImg} alt={name} />
+      </div>
+      <div>
+        <h3>{name}</h3>
+        <h3>#{idPokemon}</h3>
+      </div>
+      <div>
+        <a href={`/${id}`}>
+          <button className="button-card">Ver más</button>
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PokemonCard
+export default PokemonCard;
