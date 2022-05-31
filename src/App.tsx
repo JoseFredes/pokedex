a639e9f93a5d50bfa3de5ff6defc9328d714caac
José Fredes <jose.fredes@mayor.cl>
2022-05-31 02:26:45 -0400

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React  from 'react';
-import PokeLanding from './pages/PokeLanding';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import PokeLanding from "./pages/PokeLanding";
+import PokemonDetail from "./pages/PokemonDetailCard";
 
 function App() {
   return (
-    <div className="App">
-     <PokeLanding/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<PokeLanding />} />
+        <Route path="/:id" element={<PokemonDetail/>} />
+      </Routes>
+    </Router>
   );
 }
 
